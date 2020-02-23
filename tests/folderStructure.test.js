9a12abe227c8e1ccc7841faa5c4436f3c4e54560
Yahya Gilany <yahya.gilany@live.com>
2020-02-23 08:59:26 -0500

    additional folder structure tests

--- tests/folderStructure.test.js
+++ tests/folderStructure.test.js
@@ -1,13 +1,21 @@
-const dirTree = require("directory-tree");
+const dirTree = require(`directory-tree`);
+const _ = require(`lodash`)
 
-test('root directory is properly setup', () => {
+test(`root directory is properly setup`, () => {
   const rootTree = dirTree(`${__dirname}/..`);
+  const nodes = rootTree.children.map(node => node.name);
 
-  expect(rootTree.children.length).toBe(10);
+  expect(nodes.includes(`resources`));
+  expect(nodes.includes(`index.html`));
+  expect(rootTree.children.length).toBe(11);
 });
 
-test('resources directory is properly setup', () => {
+test(`resources directory is properly setup`, () => {
   const resourcesTree = dirTree(`${__dirname}/../resources`);
+  const nodes = resourcesTree.children.map(node => node.name);
 
+  expect(nodes.includes(`images`));
+  expect(nodes.includes(`scripts`));
+  expect(nodes.includes(`styles`));
   expect(resourcesTree.children.length).toBe(3);
 });
